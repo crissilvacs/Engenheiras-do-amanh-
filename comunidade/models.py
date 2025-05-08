6b6850629bf4d73d57190b583ab21ee55e3534a3
Fátima Cristina <fatimacristina1906@gmail.com>
2025-05-08 01:05:53 -0300

    Primeiro commit do projeto Django com funcionalidades de curtir, comentar e compartilhar

--- comunidade/models.py
+++ comunidade/models.py
@@ -1,4 +1,3 @@
-# models.py
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
@@ -6,9 +5,23 @@ from django.utils import timezone
 class Post(models.Model):
     titulo = models.CharField(max_length=100)
     conteudo = models.TextField()
-    imagem = models.ImageField(upload_to='posts/', null=True, blank=True)
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    imagem = models.ImageField(upload_to='posts/', blank=True, null=True)
     data_criacao = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.titulo
+
+class Comentario(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comentarios')
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    texto = models.TextField()
+    data = models.DateTimeField(auto_now_add=True)
+
+class Curtida(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='curtidas')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    data = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('post', 'usuario')  # Evita múltiplas curtidas do mesmo usuário
